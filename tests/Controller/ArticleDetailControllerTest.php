b138dd85f3c0023429d8b37ca0581e4266e5a70f
Samuel VIVIER <samvivier25@gmail.com>
2025-03-17 15:46:18 +0100

    Add test ArticleDetailController

--- tests/Controller/ArticleDetailControllerTest.php
+++ tests/Controller/ArticleDetailControllerTest.php
@@ -2,15 +2,47 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Article;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class ArticleDetailControllerTest extends WebTestCase
 {
-    public function testIndex(): void
-    {
-        $client = static::createClient();
-        $client->request('GET', '/article/detail');
+    private KernelBrowser $client;
+    private EntityManagerInterface $entityManager;
 
-        self::assertResponseIsSuccessful();
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
+    }
+
+    public function testArticleDetailPage()
+    {
+        // Se connecter avec un utilisateur ayant ROLE_USER (ajoute un utilisateur en base si nécessaire)
+        $this->client->loginUser($this->entityManager->getRepository(User::class)->findOneBy(['username' => 'admin']));
+
+          // Récupérer un article aléatoire depuis la base de données
+          $article = $this->entityManager->getRepository(Article::class)->findOneBy([]);
+
+          // Si aucun article n'est trouvé, échouer le test
+          if (!$article) {
+              $this->fail('Aucun article trouvé dans la base de données.');
+          }
+  
+
+        // Accéder à la page des articles
+        $this->client->request('GET', '/article/detail/' . $article->getId());
+
+        // Vérifier que la réponse est un succès (200 OK)
+        $this->assertResponseIsSuccessful();
+
+        // Vérifier que le titre de l'article et le contune sont présents dans la page
+        $this->assertSelectorTextContains('h1', $article->getTitle());
+        $this->assertSelectorTextContains('p', $article->getContent());
+
     }
 }
