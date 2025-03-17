a9d6e5b919db6d477573e8dc5e4ff6b21c01bedb
Samuel VIVIER <samvivier25@gmail.com>
2025-03-17 09:43:47 +0100

    Add modification in admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -43,5 +43,8 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Articles', 'fa fa-newspaper-o', Article::class);
         yield MenuItem::section('Aide');
+        yield MenuItem::linktoRoute('Création', 'fa fa-question', 'app_help_new');
+        yield MenuItem::linktoRoute('Editer', 'fa fa-question', 'app_help_modif');
+        yield MenuItem::linktoRoute('Détails', 'fa fa-question', 'app_help_show');
     }
 }
